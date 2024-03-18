8b0d102cf9b4367d72364e83d3f43317026ba4a3
Max Tamer-Mahoney <max@dayzerodiagnostics.com>
2024-03-18 19:21:25 -0400

    Update neovim configuration, moving it to Lua

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,175 @@
+-- Plugins
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+--[[
+Plug('bling/vim-airline')
+vim.g.airline_powerline_fonts = 1
+--]]
+
+Plug('nvim-lualine/lualine.nvim')
+
+Plug('bling/vim-bufferline')
+
+Plug('Raimondi/delimitMate')
+
+Plug('tmhedberg/SimpylFold')
+
+Plug('vim-scripts/indentpython.vim')
+
+Plug('lervag/vimtex')
+vim.g.vimtex_view_general_viewer = 'evince'
+
+Plug('tpope/vim-surround')
+
+Plug('tpope/vim-repeat')
+
+--[[
+Plug('tpope/vim-vinegar')
+vim.cmd("let g:netrw_list_hide = '\(^\|\s\s\)\zs\.\S\+'")
+--]]
+
+vim.g.python3_host_prog = '/usr/bin/python3.8'
+vim.g.polyglot_disabled = { 'python' }
+Plug('wookayin/semshi', { ['do'] = function()
+     vim.cmd["UpdateRemotePlugins"]()
+end })
+vim.g['semshi#simplify_markup'] = false
+
+Plug('ellisonleao/glow.nvim')
+
+Plug('neovim/nvim-lspconfig')
+
+Plug('sheerun/vim-polyglot')
+
+Plug('tpope/vim-sleuth')
+
+Plug('kkoomen/vim-doge')
+vim.g.doge_doc_standard_python = 'google'
+
+--[[
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = function()
+  vim.cmd["TSUpdate"]()
+end })
+--]]
+
+Plug('dstein64/vim-startuptime')
+
+vim.call('plug#end')
+
+vim.opt.number = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+
+vim.opt.showmatch = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.mouse = ''
+
+vim.cmd([[
+"Auto indenting toggle for pasting
+nnoremap <F2> :set invpaste paste?<CR>
+set pastetoggle=<F2>
+set showmode
+
+"Reformat key
+map <F7> mzgg=G`z<CR>
+]])
+
+vim.keymap.set('n', '<up>', '<nop>')
+vim.keymap.set('n', '<down>', '<nop>')
+vim.keymap.set('n', '<left>', '<nop>')
+vim.keymap.set('n', '<right>', '<nop>')
+vim.keymap.set('i', '<up>', '<nop>')
+vim.keymap.set('i', '<down>', '<nop>')
+vim.keymap.set('i', '<left>', '<nop>')
+vim.keymap.set('i', '<right>', '<nop>')
+
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
+vim.keymap.set('n', '<space>', 'za')
+
+vim.opt.clipboard = "unnamed"
+
+vim.api.nvim_set_hl(0, 'LineNr', { ctermfg = 'grey' })
+
+require('glow').setup()
+
+local lspconfig = require('lspconfig')
+lspconfig.ruff_lsp.setup{
+  init_options = {
+    settings = {
+      args = {},
+    }
+  }
+}
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+require('lualine').setup()
+
+--[[
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+
+    disable = { "c", "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+--]]
