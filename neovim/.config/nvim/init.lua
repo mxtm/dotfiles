69e3679f2d7236f91120b250c02e560337c4b2f5
Max Tamer-Mahoney <max@mxtm.me>
2024-12-18 21:16:41 -0500

    Add signature display and auto brackets for blink.cmp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -146,6 +146,8 @@ require("lazy").setup({
 	  use_nvim_cmp_as_default = true,
 	},
 	nerd_font_variant = "mono",
+	signature = { enabled = true },
+	completion = { accept = { auto_brackets = { enabled = true } } },
       },
     },
     {
