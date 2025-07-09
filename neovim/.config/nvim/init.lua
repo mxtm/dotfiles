d06dcae292353df39603d0a361d01a1095c76eb3
Max Tamer-Mahoney <max@mxtm.me>
2025-07-09 18:29:49 -0400

    Run stylua

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -65,7 +65,7 @@ require("lazy").setup({
 						},
 					},
 				}
-			end
+			end,
 		},
 		{
 			"folke/which-key.nvim",
@@ -223,8 +223,8 @@ require("lazy").setup({
 			event = "VeryLazy",
 			config = true,
 			keys = {
-			  { "<leader>gq", "<Cmd>GitConflictListQf<CR>", desc = "list conflicts (qflist)" },
-			  { "<leader>gr", "<Cmd>GitConflictRefresh<CR>", desc = "refresh conflicts (qflist)" },
+				{ "<leader>gq", "<Cmd>GitConflictListQf<CR>", desc = "list conflicts (qflist)" },
+				{ "<leader>gr", "<Cmd>GitConflictRefresh<CR>", desc = "refresh conflicts (qflist)" },
 			},
 		},
 		{
@@ -246,18 +246,18 @@ require("lazy").setup({
 			opts = { enabled = false, textwidth_mode = 1 },
 		},
 		{
-			'Bekaboo/dropbar.nvim',
+			"Bekaboo/dropbar.nvim",
 			-- optional, but required for fuzzy finder support
 			dependencies = {
-				'nvim-telescope/telescope-fzf-native.nvim',
-				build = 'make'
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
 			},
 			config = function()
-				local dropbar_api = require('dropbar.api')
-				vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-				vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-				vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
-			end
+				local dropbar_api = require("dropbar.api")
+				vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+				vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+				vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+			end,
 		},
 		{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
 		{ "fladson/vim-kitty", ft = "kitty" },
